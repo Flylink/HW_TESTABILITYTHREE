99f210e42f304157e0c573e1d07b02c902f70cc4
Flylink <flylink92@mail.ru>
2023-12-23 18:43:26 +0700

    rename varibale class CreditPaymentService

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
     public int calculate(int sum, double rate, int months) {
-        double i = rate / 12 / 100; // процентная ставка в месяц
-        double pow = Math.pow(1 + i, months); // (1 + i)^n
-        double payment = sum * (i + i / (pow - 1)); // ежемесячный платеж
+        double interestRate = rate / 12 / 100; // процентная ставка в месяц
+        double monthlyСalculation = Math.pow(1 + interestRate, months); // (1 + i)^n
+        double payment = sum * (interestRate + interestRate / (monthlyСalculation - 1)); // ежемесячный платеж
         return (int) payment;
     }
 }
